912a01fac080b390499c84e744d38d8da532370c
Amaan Jaweed <ajaweed@ur.rochester.edu>
2024-02-15 16:08:00 -0500

    accepting form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,32 @@
 export default function Login() {
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log("Submit button clicekd");
+
+    const fd = new FormData(event.target);
+    const data = Object.fromEntries(fd.entries());
+
+    console.log(data);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" required/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" required minLength={6} />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button type="reset" className="button button-flat">Reset</button>
+        <button type ="submit" className="button">Login</button>
       </p>
     </form>
   );
